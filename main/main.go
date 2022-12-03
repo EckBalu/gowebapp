1590ab335b1b430e15e2b8f6d799e956c19cbb26
EckBalu <eckbalu@gmail.com>
2022-12-03 15:55:06 +0100

    initial commit

---
+++ main/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"math"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	x := 0.01
+	for i := 0; i <= 1000; i++ {
+		x += math.Sqrt(x)
+	}
+	w.Write([]byte("Kapcsolat!"))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
